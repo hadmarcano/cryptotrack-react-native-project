97c44d940027ab8e3e3deb5dca9bf59513bcfe44
Hector Adolfo <hadmarcano@gmail.com>
2021-06-16 22:50:55 -0400

    first commit

---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class CoinsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>BIENVENIDOS a COINS SCREEN</Text>
+      </View>
+    );
+  }
+}
+
+export default CoinsScreen;
